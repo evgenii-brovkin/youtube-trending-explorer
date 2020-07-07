827963ee9e665f564fe66c7b79ea7a878127be98
Evgenii Brovkin <evgenii_brovkin@epam.com>
2020-07-07 12:28:37 +0300

    change daily routine

--- app.py
+++ app.py
@@ -1,21 +1,44 @@
-import schedule
 import os
 
-from ytexplorer.data_fetch import fetch_youtube_trending_data
-from ytexplorer.api_helper import build_drive_service, upload_file
+import schedule
+import warnings
+from trendscrap.api_helper import upload_file, exists
+from trendscrap.data_fetch import fetch_youtube_trending_data, get_file_path
 
 
 def daily_routine():
+    """
+    Fetch data, upload to the Google Drive.
+    """
     videos_df, video_data_path = fetch_youtube_trending_data(save_data=True)
-    drive_service = build_drive_service()
-    upload_file(video_data_path, f'data/youtube_trending_explorer/{os.path.split(video_data_path)[-1]}', drive_service)
-    
-    
+    n_regions = 4  # default
+    if videos_df.shape[0] != n_regions * 50:
+        warnings.warn("Number of videos is lesser than expected")
+        return
+
+    upload_file(
+        video_data_path, f"data/youtube_trending_explorer/{get_file_path()}",
+    )
+
+
+def double_check():
+    """
+    Rerun daily routine if file hasn't been uploaded to the Drive.
+    """
+    file_name = get_file_path()
+    if not exists(file_name):
+        daily_routine()
+
+
 def main():
-    schedule.every().day.at("11:00").do(daily_routine)
+    schedule.every().day.at("8:00").do(daily_routine)
+    schedule.every().day.at("11:00").do(double_check)
+    schedule.every().day.at("14:00").do(double_check)
+    schedule.every().day.at("17:00").do(double_check)
+    schedule.every().day.at("20:00").do(double_check)
     while True:
         schedule.run_pending()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
