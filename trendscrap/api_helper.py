d6f82c3f1d480b5d21eaf2f261d64d793dbdb1c0
Evgenii Brovkin <evgenii_brovkin@epam.com>
2020-07-07 13:49:54 +0300

    update api functions

--- trendscrap/api_helper.py
+++ trendscrap/api_helper.py
@@ -1,20 +1,32 @@
-import googleapiclient.discovery
+from typing import Dict, NoReturn, Any
+
 from google.oauth2 import service_account
+
+from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
 
-def build_drive_service(serice_account_credentials_file='service_account_credentials.json'):
+def build_drive_service(
+    serice_account_credentials_file: str = "service_account_credentials.json",
+):
     """
     Connect and authenticate to a Google API service account 
     using info from `serice_account_credentials_file` JSON file.
     
     Return a drive resource object for interacting with Drive API
     """
-    credentials = service_account.Credentials.from_service_account_file(serice_account_credentials_file)
-    return googleapiclient.discovery.build('drive', 'v3', credentials=credentials)
+    credentials = service_account.Credentials.from_service_account_file(
+        serice_account_credentials_file
+    )
+    return build("drive", "v3", credentials=credentials)
 
 
-def upload_file(path_to_local_file, destination_path, drive_service, file_mimetype=None):
+def upload_file(
+    path_to_local_file: str,
+    destination_path: str,
+    drive_service=None,
+    file_mimetype: str = None,
+) -> str:
     """
     Upload a file located in `path_to_local_file` to `destination_path`. 
     Destination path must be in format "parent_dir1/parent_dir2/filename".
@@ -22,26 +34,34 @@ def upload_file(path_to_local_file, destination_path, drive_service, file_mimety
     
     Returns a file_id of a newly uploaded file.
     """
-    *parent_dirs, filename = destination_path.split('/')
-    directories = drive_service.files().list(
-        q="mimeType = 'application/vnd.google-apps.folder'",
-        pageSize=10, fields="files(name, id, mimeType, parents, properties)"
-    ).execute()
-    parents = [dir['id'] for dir in directories['files'] if dir['name'] == parent_dirs[-1]]
-    file_metadata = {
-        'name': filename, 
-        'parents': parents
-    }
+    if drive_service is None:
+        drive_service = build_drive_service()
+    *parent_dirs, filename = destination_path.split("/")
+    directories = (
+        drive_service.files()
+        .list(
+            q="mimeType = 'application/vnd.google-apps.folder'",
+            pageSize=10,
+            fields="files(name)",
+        )
+        .execute()
+    )
+    parents = [
+        dir["id"] for dir in directories["files"] if dir["name"] == parent_dirs[-1]
+    ]
+    file_metadata = {"name": filename, "parents": parents}
     media = MediaFileUpload(path_to_local_file, mimetype=file_mimetype)
-    file = drive_service.files().create(
-        body=file_metadata,
-        media_body=media,
-        fields='id'
-    ).execute()
-    return file['id']
+    file = (
+        drive_service.files()
+        .create(body=file_metadata, media_body=media, fields="id")
+        .execute()
+    )
+    return file["id"]
 
 
-def upload_image(path_to_local_file, destination_path, drive_service):
+def upload_image(
+    path_to_local_file: str, destination_path: str, drive_service=None
+) -> str:
     """
     Upload a JPEG image located in `path_to_local_file` to `destination_path`. 
     Destination path must be in format "parent_dir1/parent_dir2/filename".
@@ -49,33 +69,64 @@ def upload_image(path_to_local_file, destination_path, drive_service):
     
     Returns a file_id of a newly uploaded file.
     """
-    return upload_file(path_to_local_file, destination_path, drive_service, 'image/jpeg')
+    return upload_file(
+        path_to_local_file, destination_path, drive_service, "image/jpeg"
+    )
 
 
-def delete_file(file_id, drive_service):
+def delete_file(file_id: str, drive_service=None) -> NoReturn:
+    """
+    Delete a file with `file_id`.
+    """
+    if drive_service is None:
+        drive_service = build_drive_service()
     drive_service.files().delete(fileId=file_id).execute()
 
 
-def build_youtube_service(api_key_file='api_key.txt'):
+def build_youtube_service(api_key_file: str = "api_key.txt"):
     """
     Connect and authenticate to a Youtube Data API  
     using key from `api_key_file` file.
     
-    Return a drive resource object for interacting with Youtube API
+    Return a youtube resource object for interacting with Youtube API
     """
-    with open(api_key_file, 'r') as f:
+    with open(api_key_file, "r") as f:
         api_key = f.read()
-    youtube_service = googleapiclient.discovery.build('youtube', 'v3', developerKey=api_key)
+    youtube_service = build("youtube", "v3", developerKey=api_key)
     return youtube_service
-    
-    
-def get_video_data(region='US', top_n_videos=50):
-    youtube = build_youtube_service(api_key_file='api_key.txt')
 
-    response = youtube.videos().list(
-        part="snippet,contentDetails,statistics",
-        chart="mostPopular",
-        maxResults=top_n_videos,
-        regionCode=region
-    ).execute()
+
+def get_video_data_from_trending(
+    region: str = "US", top_n_videos: int = 50
+) -> Dict[str, Any]:
+    """
+    Fetch `top_n_videos` for a `region` from a Youtube trending page.
+
+    Region must be a two character country code. Number of videos can not exceed 50.
+    """
+    youtube = build_youtube_service(api_key_file="api_key.txt")
+
+    response = (
+        youtube.videos()
+        .list(
+            part="snippet,contentDetails,statistics",
+            chart="mostPopular",
+            maxResults=top_n_videos,
+            regionCode=region,
+        )
+        .execute()
+    )
     return response
+
+
+def exists(file_name: str, drive_service=None) -> bool:
+    if drive_service is None:
+        drive_service = build_drive_service()
+    responce = (
+        drive_service.files()
+        .list(q=f"name = '{file_name}'", fields="files(name)")
+        .execute()
+    )
+    if len(responce["files"]) > 0:
+        return True
+    return False
--- trendscrap/data_fetch.py
+++ trendscrap/data_fetch.py
@@ -1,98 +1,137 @@
-import urllib.request as ur
-import pandas as pd
 import os
+from typing import Dict, Iterable, Optional, Union, Any
 
-from .api_helper import get_video_data
+import pandas as pd
+
+from .api_helper import get_video_data_from_trending
 
 
-def parse_video_data(video_data):
-    video_title = video_data['snippet']['title']
-    channel_name = video_data['snippet']['channelTitle']
+def parse_video_data(video_data: Dict[str, Any]) -> Dict[str, Union[str, int, float]]:
+    """
+    Parse a responce from Youtube API for one video.
+    
+    Return a dictionary with the video info and statistics.
+    """
+    video_title = video_data["snippet"]["title"]
+    channel_name = video_data["snippet"]["channelTitle"]
 
     channel_link_template = "https://www.youtube.com/channel/{}"
-    channel_id = video_data['snippet']['channelId']
+    channel_id = video_data["snippet"]["channelId"]
     channel_link = channel_link_template.format(channel_id)
 
-    video_link_template = 'https://www.youtube.com/watch?v={}'
-    video_id = video_data['id']
+    video_link_template = "https://www.youtube.com/watch?v={}"
+    video_id = video_data["id"]
     video_link = video_link_template.format(video_id)
 
-    published_ts = video_data['snippet']['publishedAt']
+    published_ts = video_data["snippet"]["publishedAt"]
     published_ts = pd.Timestamp(published_ts)
-    time_since_publishing = int((pd.Timestamp.now(tz=published_ts.tz) - pd.Timestamp(published_ts)).round('H').total_seconds()/3600)
-    
+    time_since_publishing = int(
+        (pd.Timestamp.now(tz=published_ts.tz) - pd.Timestamp(published_ts))
+        .round("H")
+        .total_seconds()
+        / 3600
+    )
+
     try:
-        video_thumbnail_link = video_data['snippet']['thumbnails']['standard']['url']
+        video_thumbnail_link = video_data["snippet"]["thumbnails"]["standard"]["url"]
     except KeyError:
-        possible_thumbnails_res = ['high', 'maxres', 'medium', 'default']
+        possible_thumbnails_res = ["high", "maxres", "medium", "default"]
         video_thumbnail_link = None
         for thumbnail_res in possible_thumbnails_res:
-            if thumbnail_res in video_data['snippet']['thumbnails']:
-                if 'url' in video_data['snippet']['thumbnails'][thumbnail_res]:
-                    video_thumbnail_link = video_data['snippet']['thumbnails'][thumbnail_res]['url']
+            if thumbnail_res in video_data["snippet"]["thumbnails"]:
+                if "url" in video_data["snippet"]["thumbnails"][thumbnail_res]:
+                    video_thumbnail_link = video_data["snippet"]["thumbnails"][
+                        thumbnail_res
+                    ]["url"]
                     break
     tags = None
-    if 'tags' in video_data['snippet']:
-        tags = ','.join(video_data['snippet']['tags'])
-        
+    if "tags" in video_data["snippet"]:
+        tags = ",".join(video_data["snippet"]["tags"])
+
     blocked_in_countries = None
-    if 'regionRestriction' in video_data['contentDetails']:
-        if 'blocked' in video_data['contentDetails']['regionRestriction']:
-            blocked_in_countries = ','.join(video_data['contentDetails']['regionRestriction']['blocked'])
-    
+    if "regionRestriction" in video_data["contentDetails"]:
+        if "blocked" in video_data["contentDetails"]["regionRestriction"]:
+            blocked_in_countries = ",".join(
+                video_data["contentDetails"]["regionRestriction"]["blocked"]
+            )
+
     likes_count = -1
-    if 'likeCount' in video_data['statistics']:
-        likes_count = int(video_data['statistics']['likeCount'])
+    if "likeCount" in video_data["statistics"]:
+        likes_count = int(video_data["statistics"]["likeCount"])
     dislikes_count = -1
-    if 'dislikeCount' in video_data['statistics']:
-        dislikes_count = int(video_data['statistics']['dislikeCount'])
+    if "dislikeCount" in video_data["statistics"]:
+        dislikes_count = int(video_data["statistics"]["dislikeCount"])
     views_count = -1
-    if 'viewCount' in video_data['statistics']:
-        views_count = int(video_data['statistics']['viewCount'])
+    if "viewCount" in video_data["statistics"]:
+        views_count = int(video_data["statistics"]["viewCount"])
     comments_count = -1
-    if 'commentCount' in video_data['statistics']:
-        comments_count = int(video_data['statistics']['commentCount'])
+    if "commentCount" in video_data["statistics"]:
+        comments_count = int(video_data["statistics"]["commentCount"])
 
     return {
-        'video_title': video_title,
-        'video_link': video_link,
-        'video_id': video_id,
-        'channel_link': channel_link,
-        'channel_id': channel_id,
-        'channel_name': channel_name,
-        'published_ts': published_ts,
-        'hours_since_publishing': time_since_publishing,
-        'tags': tags,
-        'likes_count': likes_count,
-        'dislikes_count': dislikes_count,
-        'views_count': views_count,
-        'comments_count': comments_count,
-        'blocked_in_countries': blocked_in_countries,
-        'video_thumbnail_link': video_thumbnail_link
+        "video_title": video_title,
+        "video_link": video_link,
+        "video_id": video_id,
+        "channel_link": channel_link,
+        "channel_id": channel_id,
+        "channel_name": channel_name,
+        "published_ts": published_ts,
+        "hours_since_publishing": time_since_publishing,
+        "tags": tags,
+        "likes_count": likes_count,
+        "dislikes_count": dislikes_count,
+        "views_count": views_count,
+        "comments_count": comments_count,
+        "blocked_in_countries": blocked_in_countries,
+        "video_thumbnail_link": video_thumbnail_link,
     }
 
 
-def compose_dataframe(region, top_n_videos=50):
-    """Get video data for `top_n_videos` in a `region` and compose a dataframe for each video from Youtube trending page"""
-    response = get_video_data(region=region, top_n_videos=top_n_videos)
-    videos_df = pd.DataFrame(
-        list(map(parse_video_data, response['items']))
-    ).assign(trending_position=lambda x: x.index.values+1) \
-     .assign(region=lambda x: region).assign(created_ts=[pd.Timestamp.now()] * top_n_videos).set_index('video_id')
+def compose_dataframe(region: str, top_n_videos: int = 50) -> pd.DataFrame:
+    """
+    Get video data for `top_n_videos` in a `region` and 
+    compose a dataframe from each video from Youtube trending page
+
+    Return a pandas Dataframe.
+    """
+    data = get_video_data_from_trending(region=region, top_n_videos=top_n_videos)
+    videos_df = (
+        pd.DataFrame(list(map(parse_video_data, data["items"])))
+        .assign(trending_position=lambda x: x.index.values + 1)
+        .assign(region=lambda x: region)
+        .assign(created_ts=[pd.Timestamp.now()] * top_n_videos)
+        .set_index("video_id")
+    )
     return videos_df
 
 
-def fetch_thumbnail_picture(thumbnail_link, filename):
-    ur.urlretrieve(thumbnail_link, filename)
+def get_file_path(dir: Optional[str] = None) -> str:
+    """
+    Return a video data file path with current date. 
+    Set `dir` as a parent directories if provided.
+
+    Return a generated path
+    """
+    now_ts = pd.Timestamp.now().date()
+    if dir is None:
+        return f"video_data_{now_ts}.csv"
+    return os.path.join(dir, f"video_data_{now_ts}.csv")
 
 
-def fetch_youtube_trending_data(save_data=True):
-    regions = ['US', 'GB', 'RU', 'DE']
+def fetch_youtube_trending_data(
+    regions: Iterable[str] = ("US", "GB", "RU", "DE"), save_data: bool = True
+) -> pd.DataFrame:
+    """
+    Get data from Youtube trending page for every video on the page (50 videos) 
+    for every region in `regions`.
+
+    Return a pandas Dataframe.
+    """
     videos_df = pd.concat(list(map(lambda x: compose_dataframe(x, 50), regions)))
     if save_data:
-        now_ts = pd.Timestamp.now().date()
-        if not os.path.exists('data'):
-            os.mkdir('data')
-        videos_df.to_csv(f'data/video_data_{now_ts}.csv')
-        return videos_df, f'data/video_data_{now_ts}.csv'
+        if not os.path.exists("data"):
+            os.mkdir("data")
+        save_to_path = get_file_path("data")
+        videos_df.to_csv(save_to_path)
+        return videos_df, save_to_path
     return videos_df
